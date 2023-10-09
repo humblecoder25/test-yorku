f6002733d7a5abca2d7d71daf0765bb537d4cf05
SMannion YorkU <smannion@yorku.ca>
2023-10-09 08:46:39 -0400

    First Commit

---
+++ main.tf
@@ -0,0 +1,22 @@
+	terraform {
+  required_version = ">= 1.5.0, < 2.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "firstEC2" {
+  ami           = "ami-0d6927ccef429da8c"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "first_tf_EC2"
+  }
+}
