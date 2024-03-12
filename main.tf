b3f648895f661a9935700e576d54b5f7a19b8bfa
humblecoder25 <soumya10chakraborty@gmail.com>
2024-03-11 21:26:29 -0400

    Name changes

--- main.tf
+++ main.tf
@@ -13,10 +13,12 @@ provider "aws" {
   region = "us-east-1"
 }
 
+
+
 resource "aws_instance" "firstEC2" {
   ami           = "ami-0d6927ccef429da8c"
   instance_type = "t2.micro"
   tags = {
-    Name = "first_tf_EC2"
+    Name = "Soumya_tf_EC2"
   }
 }
